a428e16b2c7c8784a43088530ea2a345e06f5b1e
Irakli Gozalishvili <rfobic@gmail.com>
2010-03-10 04:05:27 +0100

    renaming resource to resources

--- resources/datastore/example.sql
+++ resources/datastore/example.sql
@@ -17,3 +17,13 @@ Table from Irakli Gozalishvili",
 
 use "http://gozala.github.com/taskhub/resource/github.gists.xml";
 select * from github.gists where user = "gozala" and description = "metadocs"
+
+use "http://gozala.github.com/taskhub/resource/github.gists.xml";
+UPDATE github.gists SET content="yql content" WHERE
+    user="gozala"
+AND
+    token="aa8610a73390ea8b6ae9216589494ef9"
+AND
+    name="test1.js"
+AND
+    id="ca86cb060484ba5dffd1"
